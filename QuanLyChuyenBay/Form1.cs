07016fc718db35cf469329b8e38be49feae39d49
Kennie291 <tqdung29186@gmail.com>
2023-06-01 07:37:27 +0700

    MH_Nhận Lịch

---
+++ QuanLyChuyenBay/Form1.cs
@@ -0,0 +1,45 @@
+namespace QuanLyChuyenBay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_NhanLich_Click(object sender, EventArgs e)
+        {
+            MH_NhanLich mhnl = new MH_NhanLich();
+            mhnl.Show();
+        }
+
+        private void btn_BanVe_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_DatCho_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btn_TraCuu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doanhThuThang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void doanhThuNam_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grevLichBay_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QuanLyChuyenBay/MH_NhanLich.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace QuanLyChuyenBay
+{
+    partial class MH_NhanLich
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lb_NhanLich = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            btn_LuuLichBay = new Button();
+            btn_ThemLichBay = new Button();
+            btn_ThoatLichBay = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // lb_NhanLich
+            // 
+            lb_NhanLich.AutoSize = true;
+            lb_NhanLich.Location = new Point(107, 20);
+            lb_NhanLich.Name = "lb_NhanLich";
+            lb_NhanLich.Size = new Size(127, 15);
+            lb_NhanLich.TabIndex = 0;
+            lb_NhanLich.Text = "Nhận Lịch Chuyến Bay";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Mã Chuyến Bay";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Sân Bay Đi";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Sân Bay Đến";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 169);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Ngày Giờ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 202);
+            label5.Name = "label5";
+            label5.Size = new Size(79, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Thời Gian Bay";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 242);
+            label6.Name = "label6";
+            label6.Size = new Size(122, 15);
+            label6.TabIndex = 11;
+            label6.Text = "Số Lượng Ghế Hạng 1";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 277);
+            label7.Name = "label7";
+            label7.Size = new Size(122, 15);
+            label7.TabIndex = 12;
+            label7.Text = "Số Lượng Ghế Hạng 2";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 307);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(371, 95);
+            dataGridView1.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(191, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(192, 23);
+            textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(191, 83);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(192, 23);
+            textBox2.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(191, 125);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(192, 23);
+            textBox3.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(191, 198);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(192, 23);
+            textBox5.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(191, 234);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(192, 23);
+            textBox6.TabIndex = 19;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(191, 269);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(192, 23);
+            textBox7.TabIndex = 20;
+            // 
+            // btn_LuuLichBay
+            // 
+            btn_LuuLichBay.Location = new Point(27, 415);
+            btn_LuuLichBay.Name = "btn_LuuLichBay";
+            btn_LuuLichBay.Size = new Size(75, 23);
+            btn_LuuLichBay.TabIndex = 21;
+            btn_LuuLichBay.Text = "Lưu";
+            btn_LuuLichBay.UseVisualStyleBackColor = true;
+            // 
+            // btn_ThemLichBay
+            // 
+            btn_ThemLichBay.Location = new Point(159, 415);
+            btn_ThemLichBay.Name = "btn_ThemLichBay";
+            btn_ThemLichBay.Size = new Size(75, 23);
+            btn_ThemLichBay.TabIndex = 22;
+            btn_ThemLichBay.Text = "Thêm mới";
+            btn_ThemLichBay.UseVisualStyleBackColor = true;
+            // 
+            // btn_ThoatLichBay
+            // 
+            btn_ThoatLichBay.Location = new Point(281, 415);
+            btn_ThoatLichBay.Name = "btn_ThoatLichBay";
+            btn_ThoatLichBay.Size = new Size(75, 23);
+            btn_ThoatLichBay.TabIndex = 23;
+            btn_ThoatLichBay.Text = "Thoát";
+            btn_ThoatLichBay.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(191, 161);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(192, 23);
+            dateTimePicker1.TabIndex = 24;
+            // 
+            // MH_NhanLich
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = SystemColors.ControlLightLight;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(395, 450);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(btn_ThoatLichBay);
+            Controls.Add(btn_ThemLichBay);
+            Controls.Add(btn_LuuLichBay);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lb_NhanLich);
+            Name = "MH_NhanLich";
+            Text = "Nhận Lịch Chuyến Bay";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lb_NhanLich;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private Button btn_LuuLichBay;
+        private Button btn_ThemLichBay;
+        private Button btn_ThoatLichBay;
+        private DateTimePicker dateTimePicker1;
+    }
+}
